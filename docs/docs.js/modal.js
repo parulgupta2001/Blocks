71d1462605d7d2aa615813016a68c934d1f1fc9c
Parul Gupta <guptaparul1002@gmail.com>
2022-03-26 10:41:05 +0530

    modal

---
+++ docs/docs.js/modal.js
@@ -0,0 +1,12 @@
+var modal = document.querySelector(".modal");
+var btn = document.querySelector("#myBtn");
+var span = document.querySelector(".close");
+
+
+btn.addEventListener("click", () => 
+    modal.style.display = "block")
+
+
+
+span.addEventListener("click",()=> modal.style.display = "none") 
+
